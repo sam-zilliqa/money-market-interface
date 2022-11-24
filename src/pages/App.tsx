26d9123caa2e54b2e92ebd04c003a8cb3d1c7c3c
sam-zilliqa <sam@zilliqa.com>
2022-11-24 10:35:13 +0700

    added password protection to the app

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,7 @@
 import { Layout, ResetScrollOnRouteChange } from 'components';
 import React from 'react';
+import Protect from 'react-app-protect';
+import 'react-app-protect/dist/index.css';
 import { QueryClientProvider } from 'react-query';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
@@ -26,48 +28,54 @@ import Xvs from 'pages/Xvs';
 import { MuiThemeProvider } from 'theme/MuiThemeProvider';
 
 const App = () => (
-  <Web3Wrapper>
-    <QueryClientProvider client={queryClient}>
-      <MuiThemeProvider>
-        <AuthProvider>
-          <SuccessfulTransactionModalProvider>
-            <DisableLunaUstWarningProvider>
-              <BrowserRouter>
-                <ToastContainer />
-                <Layout>
-                  <ResetScrollOnRouteChange />
+  <Protect sha512="197b956da94b7f32074c6a94a4ce1218110017c8002bdf488aea0394c4f119984544d2c448ffcd0264baae12ebd44ca59c350ab78807c805d01e9149ac91027c">
+    <Web3Wrapper>
+      <QueryClientProvider client={queryClient}>
+        <MuiThemeProvider>
+          <AuthProvider>
+            <SuccessfulTransactionModalProvider>
+              <DisableLunaUstWarningProvider>
+                <BrowserRouter>
+                  <ToastContainer />
+                  <Layout>
+                    <ResetScrollOnRouteChange />
 
-                  <Switch>
-                    <Route exact path={Path.ROOT} component={Dashboard} />
+                    <Switch>
+                      <Route exact path={Path.ROOT} component={Dashboard} />
 
-                    <Route exact path={Path.MARKETS} component={Markets} />
-                    <Route exact path={Path.MARKET_DETAILS} component={MarketDetails} />
+                      <Route exact path={Path.MARKETS} component={Markets} />
+                      <Route exact path={Path.MARKET_DETAILS} component={MarketDetails} />
 
-                    <Route exact path={Path.VAULTS} component={Vaults} />
+                      <Route exact path={Path.VAULTS} component={Vaults} />
 
-                    <Route exact path={Path.HISTORY} component={History} />
+                      <Route exact path={Path.HISTORY} component={History} />
 
-                    <Route exact path={Path.GOVERNANCE} component={Vote} />
-                    <Route exact path={Path.GOVERNANCE_LEADER_BOARD} component={VoterLeaderboard} />
-                    <Route exact path={Path.GOVERNANCE_ADDRESS} component={VoterDetails} />
-                    <Route exact path={Path.GOVERNANCE_PROPOSAL_DETAILS} component={Proposal} />
+                      <Route exact path={Path.GOVERNANCE} component={Vote} />
+                      <Route
+                        exact
+                        path={Path.GOVERNANCE_LEADER_BOARD}
+                        component={VoterLeaderboard}
+                      />
+                      <Route exact path={Path.GOVERNANCE_ADDRESS} component={VoterDetails} />
+                      <Route exact path={Path.GOVERNANCE_PROPOSAL_DETAILS} component={Proposal} />
 
-                    <Route exact path={Path.XVS} component={Xvs} />
+                      <Route exact path={Path.XVS} component={Xvs} />
 
-                    <Route exact path={Path.CONVERT_VRT} component={ConvertVrt} />
+                      <Route exact path={Path.CONVERT_VRT} component={ConvertVrt} />
 
-                    <Route exact path={Path.SWAP} component={Swap} />
+                      <Route exact path={Path.SWAP} component={Swap} />
 
-                    <Redirect to={Path.ROOT} />
-                  </Switch>
-                </Layout>
-              </BrowserRouter>
-            </DisableLunaUstWarningProvider>
-          </SuccessfulTransactionModalProvider>
-        </AuthProvider>
-      </MuiThemeProvider>
-    </QueryClientProvider>
-  </Web3Wrapper>
+                      <Redirect to={Path.ROOT} />
+                    </Switch>
+                  </Layout>
+                </BrowserRouter>
+              </DisableLunaUstWarningProvider>
+            </SuccessfulTransactionModalProvider>
+          </AuthProvider>
+        </MuiThemeProvider>
+      </QueryClientProvider>
+    </Web3Wrapper>
+  </Protect>
 );
 
 export default App;
