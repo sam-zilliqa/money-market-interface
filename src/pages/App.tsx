6db2b5c24fe43fa47e4882ea228617297df4c867
sam-zilliqa <sam@zilliqa.com>
2022-11-24 11:19:07 +0700

    disabled the pages we wont support

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -13,18 +13,18 @@ import Path from 'constants/path';
 import { AuthProvider } from 'context/AuthContext';
 import { DisableLunaUstWarningProvider } from 'context/DisableLunaUstWarning';
 import { SuccessfulTransactionModalProvider } from 'context/SuccessfulTransactionModalContext';
-import ConvertVrt from 'pages/ConvertVrt';
+// import ConvertVrt from 'pages/ConvertVrt';
 import Dashboard from 'pages/Dashboard';
 import History from 'pages/History';
 import Markets from 'pages/Market';
 import MarketDetails from 'pages/MarketDetails';
-import Proposal from 'pages/Proposal';
-import Swap from 'pages/Swap';
-import Vaults from 'pages/Vault';
-import Vote from 'pages/Vote';
-import VoterDetails from 'pages/VoterDetails';
-import VoterLeaderboard from 'pages/VoterLeaderboard';
-import Xvs from 'pages/Xvs';
+// import Proposal from 'pages/Proposal';
+// import Swap from 'pages/Swap';
+// import Vaults from 'pages/Vault';
+// import Vote from 'pages/Vote';
+// import VoterDetails from 'pages/VoterDetails';
+// import VoterLeaderboard from 'pages/VoterLeaderboard';
+// import Xvs from 'pages/Xvs';
 import { MuiThemeProvider } from 'theme/MuiThemeProvider';
 
 const App = () => (
@@ -45,12 +45,10 @@ const App = () => (
 
                       <Route exact path={Path.MARKETS} component={Markets} />
                       <Route exact path={Path.MARKET_DETAILS} component={MarketDetails} />
-
-                      <Route exact path={Path.VAULTS} component={Vaults} />
-
                       <Route exact path={Path.HISTORY} component={History} />
 
-                      <Route exact path={Path.GOVERNANCE} component={Vote} />
+                      {/* <Route exact path={Path.VAULTS} component={Vaults} /> */}
+                      {/* <Route exact path={Path.GOVERNANCE} component={Vote} />
                       <Route
                         exact
                         path={Path.GOVERNANCE_LEADER_BOARD}
@@ -58,12 +56,9 @@ const App = () => (
                       />
                       <Route exact path={Path.GOVERNANCE_ADDRESS} component={VoterDetails} />
                       <Route exact path={Path.GOVERNANCE_PROPOSAL_DETAILS} component={Proposal} />
-
                       <Route exact path={Path.XVS} component={Xvs} />
-
                       <Route exact path={Path.CONVERT_VRT} component={ConvertVrt} />
-
-                      <Route exact path={Path.SWAP} component={Swap} />
+                      <Route exact path={Path.SWAP} component={Swap} /> */}
 
                       <Redirect to={Path.ROOT} />
                     </Switch>
